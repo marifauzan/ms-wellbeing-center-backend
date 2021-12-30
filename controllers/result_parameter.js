429f23e2fc1bab466961f8ae74539eb3040df791
Deni Alpiana <Denzalv01@gmail.com>
2021-12-30 22:29:56 +0700

    add controllers: result_parameter

---
+++ controllers/result_parameter.js
@@ -0,0 +1,135 @@
+const db = require("../models");
+const ResultParam = db.result_parameter;
+
+exports.findAll = (req, res) => {
+  const conditions = req.query.conditions;
+  let condition = conditions
+    ? { conditions: { $regex: new RegExp(conditions), $options: "i" } }
+    : {};
+
+  ResultParam.find(condition)
+    .then((data) => {
+      res.send({
+        success: true,
+        message: "Data found",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: err.message || "Some error occured while retrieving ResultParam.",
+        data: {},
+      });
+    });
+};
+
+exports.create = (req, res) => {
+  if (!req.body.username) {
+    res
+      .status(400)
+      .send({ success: false, message: "Content can not be empty!" });
+    return;
+  }
+
+  ResultParam.create(req.body)
+    .then((data) => {
+      res.send({
+        success: true,
+        message: "Data has been Submitted successfully",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: err.message || "Some error occured while creating the ResultParams",
+        data: {},
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  ResultParam.findById(id)
+    .then((data) => {
+      if (!data)
+        res.status(404).send({
+          success: false,
+          message: "Not found ResultParam with id " + id,
+        });
+      else
+        res.send({
+          success: true,
+          message: "Data found",
+          data: data,
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: "Error retrieving ResultParams with id=" + id,
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      success: false,
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const id = req.params.id;
+
+  ResultParam.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          success: false,
+          message: `Cannot update ResultParams with id=${id}. Maybe ResultParams was not found!`,
+          data: {},
+        });
+      } else
+        res.send({
+          success: true,
+          message: "ResultParams was updated successfuly.",
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: "Error updating ResultParams with id=" + id,
+        data: {},
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  ResultParam.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          success: false,
+          message: `Cannot delete ResultParams with id=${id}. Maybe ResultParams was not found!`,
+          data: {},
+        });
+      } else {
+        res.send({
+          success: true,
+          message: "ResultParams was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: "Could not delete ResultParams with id=" + id,
+        data: {},
+      });
+    });
+};
