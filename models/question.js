07e9c6c1577a1b7234a9366948da62e60720940c
Mar'i Fauzan <marifauzan20@gmail.com>
2022-01-03 22:35:18 +0700

    fix bug: model question.js

--- models/question.js
+++ models/question.js
@@ -1,14 +1,15 @@
 module.exports = (mongoose) => {
   let questionsSchema = mongoose.Schema({
-    answer: {
-      type: mongoose.Types.ObjectId,
-      ref: "answers",
-    },
     question: {
       type: String,
       required: true,
     },
-    choices: [1, 2, 3],
+    choices: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "answers",
+      },
+    ],
   });
 
   const Question = mongoose.model("question", questionsSchema);
